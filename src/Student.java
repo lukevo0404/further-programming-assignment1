6f45cf6a34c6d50e9764fab9eb46c80a73b9d88f
lukevo4 <trancongnguyenvo@gmail.com>
2022-03-29 11:40:02 +0700

    upload

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    private String id;
+    private String name;
+    private String birthdate;
+
+    public Student(){
+        this.id = "default";
+        this.name = "default";
+        this.birthdate = "default";
+    }
+
+    public
+
+}
