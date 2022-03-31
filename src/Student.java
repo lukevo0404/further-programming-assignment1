7eea0d2eb64fe72a59140a099afed23b8e3c8d23
lukevo4 <trancongnguyenvo@gmail.com>
2022-04-01 04:58:58 +0700

    Student Class created

--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,47 @@
+import java.util.Date;
+
 public class Student {
     private String id;
+    private Date birthdate;
     private String name;
-    private String birthdate;
 
-    public Student(){
-        this.id = "default";
-        this.name = "default";
-        this.birthdate = "default";
+    public Student() {
+        super();
     }
 
-    public
+    public Student(String id, Date birthdate, String name ) {
+        super();
+        this.id = id;
+        this.birthdate = birthdate;
+        this.name = name;
+    }
 
+    public String getId(){
+        return id;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id: " + id + ", birthdate: " + birthdate + ", name:" + name + "]";
+    }
 }
